9a8135e8b4e9b4ef88d05f75942d299533519fb2
zucker1567 <johan.romo.2000@gmail.com>
2025-06-02 10:13:13 -0400

    actualicazion

---
+++ miniproyecto/tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    theme: {
+    extend: {
+    colors: {
+        dark: "#0D1117",
+        light: "#FFFFFF",
+        highlight: "#E3FF6A",
+        neonGreen: "#A4FF7A",
+        deepBlue: "#1E293B",
+        softGray: "#8892B0",
+        },
+    backgroundImage: {
+        "gradient-main": "linear-gradient(135deg, #1E293B 0%, #0D1117 100%)",
+        },
+    },
+    },
+    plugins: [],
+};
